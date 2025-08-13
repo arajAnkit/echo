51c383fab66d88e98d33b30579d3802b7c2ff818
Ankit Rajpoot <marketing.araj@gmail.com>
2025-08-13 14:27:13 +0530

    01: (Added): Setup

---
+++ apps/widget/app/page.tsx
@@ -0,0 +1,14 @@
+import { add } from "@workspace/math/add";
+import { Button } from "@workspace/ui/components/button";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello World / widget</h1>
+        <Button size="sm">Button</Button>
+        <p className="text-2xl">{add(4, 5)}</p>
+      </div>
+    </div>
+  );
+}
